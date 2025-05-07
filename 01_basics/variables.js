1eb704899c15985fc0d41b3f8709c71170fa771d
prajeevranjan <prajeevranjan1680@gmail.com>
2025-05-07 20:18:10 +0530

    used let, const and var

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "rajeev@gmail.com"
+var accountPassword = "Raj12345"
+accountCity = "bokaro"
+
+// accountId = 23  // accountId is a const so this will be uneffective.
+accountPassword = "29110"
+accountEmail = "anc@gmail.com"
+accountCity = "nkaak"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
