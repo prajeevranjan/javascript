bdfcbf50df489ceb624ec167e10ebd5285950643
prajeevranjan <prajeevranjan1680@gmail.com>
2025-05-23 18:04:13 +0530

    datatypes done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,28 @@
+"use Strict";   // treat all code as newer version
+
+//alert(3 + 3)  // we are using nodejs not browser
+
+
+
+let name = "rajeev" // string data tpes  
+let age = 20  // number data types
+let isLoggedIn = false   // boolean data type
+let state
+
+
+
+//    ----primitive data type ------
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness
+
+
+// object
+
+console.log(typeof "rajeev");
+console.log(typeof null);  // output object
+console.log(typeof undefined);  // output undefined
