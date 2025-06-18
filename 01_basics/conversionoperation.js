0cf14546fa6a8adbd95086b07977318a2d6d9ca5
prajeevranjan <prajeevranjan1680@gmail.com>
2025-06-18 20:36:51 +0530

    summation of strings and integers

--- 01_basics/conversionoperation.js
+++ 01_basics/conversionoperation.js
@@ -35,3 +35,21 @@ let value = 3
 let negvalue = -value
 console.log(value)
 console.log(negvalue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+
+let str1 = "hello "
+let str2 = "rajeev"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log(1 + 2);
+console.log("1" + 2 + 2);
+console.log(1+ 2 + "2");
+
